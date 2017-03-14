aedad87e28b699189b249457350f5917dbdb9cd1
PrzemyslawWoldon <przemyslawwoldon@gmail.com>
2017-03-14 21:17:27 +0100

    3.1.12a klasy ktore nie musza byc publiczne

--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.stacks.list;
 
-public class Node {
+class Node {
 
 	private final int value;//w miejscu odwolania sie do tego atrybutu pojawila sie metoda
 	private Node prev;//w miejscu odwolania sie do tego atrybutu pojawila sie metoda
